457fc43a3554873cd577198a4259683eca149d99
lucasmoraist <luksmnt1101@gmail.com>
2025-09-20 10:06:05 -0300

    feat: add validation for daysWorked in CalculatePayment use case

--- src/main/java/com/codelab/wp_payroll/application/usecases/payment/CalculatePayment.java
+++ src/main/java/com/codelab/wp_payroll/application/usecases/payment/CalculatePayment.java
@@ -1,14 +1,21 @@
 package com.codelab.wp_payroll.application.usecases.payment;
 
 import com.codelab.wp_payroll.application.gateway.ClientGateway;
+import com.codelab.wp_payroll.domain.exception.DaysException;
 import com.codelab.wp_payroll.domain.model.Payment;
 import com.codelab.wp_payroll.domain.model.Worker;
 
 public record CalculatePayment(ClientGateway clientGateway) {
 
     public Payment execute(long workerId, int daysWorked) {
+        if (daysWorked <= 0) {
+            throw new DaysException("Days worked must be greater than zero");
+        }
+
         Worker worker = clientGateway.getWorkerById(workerId);
+        
         Double totalPayment = worker.dailyIncome() * daysWorked;
+
         return new Payment(worker.name(), worker.dailyIncome(), daysWorked, totalPayment);
     }
 
