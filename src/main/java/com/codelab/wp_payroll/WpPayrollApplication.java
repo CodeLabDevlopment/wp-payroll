f24f50e6a45170948437ff1d4195ed4fd251cbc0
lucasmoraist <luksmnt1101@gmail.com>
2025-09-19 23:03:39 -0300

    first commit

---
+++ src/main/java/com/codelab/wp_payroll/WpPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpPayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/wp_payroll/WpPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
